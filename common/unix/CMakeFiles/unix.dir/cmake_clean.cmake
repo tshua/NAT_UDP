cebe88beb5dde760e02741bebc64b40af4b94bb2
lifeibiren <lifeibiren@qq.com>
2016-09-06 16:42:45 +0800

    Remove cmake caches.

--- common/unix/CMakeFiles/unix.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/unix.dir/sock.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/unix.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
